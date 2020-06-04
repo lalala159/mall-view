4153cd4e889361ffc7522076df30f2482eecb04e
hecheng <429320998@qq.com>
2020-06-04 09:11:12 +0800

    登陆校验

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import {server} from './utils/http'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+
+
+// 安装路由
+Vue.use(VueRouter)
+
+Vue.use(ElementUI)
+
+Vue.use(VueAxios, axios)
+
+Vue.prototype.$server=server;
+
+
+
+new Vue({
+  router,
+  el: '#app',
+  render: h => h(App)
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.path.startsWith('/login')) {
+        window.localStorage.removeItem('access_token');    
+        next()
+    } else {
+        let user = window.localStorage.getItem('access_token');
+		console.log(user);
+        if (!user) {
+            next({ path: '/login' })
+        } else {
+            next()
+        }
+    }
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,100 @@
+<template>
+	<div>
+		<el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
+			<h3 class="login-title">欢迎登录</h3>
+			<el-form-item label="账号" prop="username">
+				<el-input type="text" placeholder="请输入账号" v-model="form.username" />
+			</el-form-item>
+			<el-form-item label="密码" prop="password">
+				<el-input type="password" placeholder="请输入密码" v-model="form.password" />
+			</el-form-item>
+			<section class="login-btn">
+				<el-button type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>
+			</section>
+		</el-form>
+	</div>
+</template>
+<script>
+	export default {
+		name: "Login",
+		data() {
+			return {
+				form: {
+					username: '',
+					password: ''
+				},
+				// 表单验证，需要在 el-form-item 元素中增加 prop 属性
+				rules: {
+					username: [{
+						required: true,
+						message: '账号不可为空',
+						trigger: 'blur'
+					}],
+					password: [{
+						required: true,
+						message: '密码不可为空',
+						trigger: 'blur'
+					}]
+				}
+			}
+		},
+		methods: {
+			onSubmit(formName) {
+				// 为表单绑定验证功能
+				this.$refs[formName].validate((valid) => {
+					if (valid) {
+						let data = {
+							username:this.form.username,
+							password:this.form.password
+						}
+						let params = {
+							scope:'read',
+							client_id:'gateway',
+							grant_type:'password',
+							client_secret:'123456',
+							username:this.form.username,
+							password:this.form.password
+						}
+						let url = 'http://localhost:9527/auth/oauth/token?username=哈哈&password=hc7777777&scope=read&client_id=gateway&grant_type=password&client_secret=123456';
+						this.$server.post(url).then(data => {
+							console.log(data)
+						})
+						this.$http.post(url).then((response) => {
+						  var data = response.data
+						  var token = data.access_token;
+						  console.log(token);
+						  window.localStorage.setItem('access_token',token)
+						  this.$router.push("/main");
+						}, error => {
+							this.$message('账号或密码错误');
+						})
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.login-box {
+		border: 1px solid #DCDFE6;
+		width: 350px;
+		margin: 180px auto;
+		padding: 35px 35px 15px 35px;
+		border-radius: 5px;
+		-webkit-border-radius: 5px;
+		-moz-border-radius: 5px;
+		box-shadow: 0 0 25px #909399;
+	}
+
+	.login-title {
+		text-align: center;
+		margin: 0 auto 40px auto;
+		color: #303133;
+	}
+
+	.login-btn {
+		text-align: center;
+		margin: 0 auto 10px auto;
+	}
+</style>
